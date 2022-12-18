e23fbb08d05c9557f0ffd68a727b18cfc1481023
Aditya Kumar <72451094+adityakumar1990@users.noreply.github.com>
2022-12-18 21:53:40 +0530

    Create DB_REFRESH.py

---
+++ DB_REFRESH.py
@@ -0,0 +1,19 @@
+import sys
+
+print(f"Name of python script is {sys.argv[0]}")
+
+if sys.argv[1]=="dev":
+    print("Deploying the code in dev")
+elif sys.argv[1]=="sit":
+    print("Deploying the code in dev")
+elif sys.argv[1]=="uat":
+    print("Deploying the code in dev")
+elif sys.argv[1]=="prod":
+    print("Deploying the code in dev")
+else :
+    print("Deploying the other code in not respected env")
+
+if sys.argv[2]:
+    print("In case of true!!")
+else:
+    print("in case of failure!!!")
